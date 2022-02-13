790eb0a73b8046da7b04728909f1f6bb3f62bc43
Michael Engelhart <angleheart@protonmail.com>
2022-02-13 12:31:35 -0500

    Boilerplate app

---
+++ src/main/java/config/Config.java
@@ -0,0 +1,66 @@
+package config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+
+    private final int serverPort;
+    private final String databaseUrl;
+    private final String databaseName;
+    private final String databaseUsername;
+    private final String databasePassword;
+
+    private Config(
+            int serverPort,
+            String databaseUrl,
+            String databaseName,
+            String databaseUsername,
+            String databasePassword) {
+        this.serverPort = serverPort;
+        this.databaseUrl = databaseUrl;
+        this.databaseName = databaseName;
+        this.databaseUsername = databaseUsername;
+        this.databasePassword = databasePassword;
+    }
+
+    private static Config instance;
+
+    public static Config getInstance() {
+        return instance;
+    }
+
+    public static Config getInstance(String path) throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(path));
+        instance = new Config(
+                Integer.parseInt(properties.getProperty("serverPort")),
+                properties.getProperty("databaseUrl"),
+                properties.getProperty("databaseName"),
+                properties.getProperty("databaseUsername"),
+                properties.getProperty("databasePassword")
+        );
+        return instance;
+    }
+
+    public DataSource getDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(databaseUrl + "/" + databaseName);
+        config.setUsername(databaseUsername);
+        config.setPassword(databasePassword);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        return new HikariDataSource(config);
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+}
---
+++ src/main/java/server/Server.java
@@ -0,0 +1,15 @@
+package server;
+
+import static spark.Spark.port;
+
+public class Server {
+
+    public static void init(int port){
+        port(port);
+
+
+
+
+    }
+
+}
